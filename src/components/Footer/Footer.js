be02d42a60bb418d2952e529f8d95f313b40b828
Sean <nortonseanm@gmail.com>
2019-04-01 16:11:54 -0400

    fix footer for mobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
     return(
         <div className={classes.Footer}>
             <div>&reg; 2016 Dirty Dogs all rights reserved </div>
-            <div>
+            <div style={{textAlign:'center'}}>
                 271 Marconi Blvd. Columbus, Ohio 43215 
                 &nbsp;|&nbsp; 614.538.0095 
-                &nbsp;|&nbsp; Contact Us
+                &nbsp;|&nbsp; Contact&nbsp;Us
             </div>
         </div>
     );
